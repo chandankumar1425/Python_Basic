356c5faf2bb3832a8cb1c0b51c33e35d8c0fcc08
Chandan Kumar Behera <118505620+chandankumar1425@users.noreply.github.com>
2023-08-05 13:47:51 +0530

    PythonS4

--- set3.py
+++ set3.py
@@ -22,3 +22,81 @@ nums = [2, 7, 11, 15]
 target = 9
 result = two_sum(nums, target)
 print(result)
+
+
+##4
+input_string = "madam"
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")  # Convert to lowercase and remove spaces
+    return s == s[::-1]
+if is_palindrome(input_string):
+    print(f"The word {input_string} is a palindrome.")
+else:
+    print(f"The word {input_string} is not a palindrome.")
+
+##5
+# from queue import Queue
+# class StackUsingQueue:
+#     def __init__(self):
+#         self.queue1 = Queue()
+#         self.queue2 = Queue()
+#     def push(self, value):
+#         self.queue1.put(value)
+#     def pop(self):
+#         if self.queue1.empty():
+#             return None
+#         while self.queue1.qsize() > 1:
+#             self.queue2.put(self.queue1.get())
+#         popped_value = self.queue1.get()
+#         self.queue1, self.queue2 = self.queue2, self.queue1  # Swap the queues
+#         return popped_value
+# stack = StackUsingQueue()
+# stack.push(1)
+# stack.push(2)
+# print(stack.pop())
+# stack.push(3)
+# print(stack.pop())
+# print(stack.pop())
+
+#//This was wrong
+
+##6
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+##7
+arr = [64, 25, 12, 22, 11]
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+selection_sort(arr)
+print(arr)
+
+##8
+# def count_words(file_path):
+#     with open(file_path, 'r') as file:
+#         content = file.read()
+#         words = content.split()
+#         return len(words)
+# input_file_path = "input.txt"
+# output_file_path = "output.txt"
+# word_count = count_words(input_file_path)
+# output_content = f"Number of words: {word_count}"
+# with open(output_file_path, 'w') as output_file:
+#     output_file.write(output_content)
+# print(f"Number of words counted: {word_count}")
+# print(f"Result written to {output_file_path}")
+#//Dont Understand
+
+
--- set4.py
+++ set4.py
@@ -6,3 +6,45 @@ word1 = "cinema"
 word2 = "iceman"
 result = are_anagrams(word1, word2)
 print(result)
+
+##2
+arr = [64, 34, 25, 12, 22, 11, 90]
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+bubble_sort(arr)
+print(arr)
+
+##3
+strs = ["flower", "flow", "flight"]
+def longest(strs):
+    if not strs:
+        return ""
+    min_str = min(strs, key=len)
+    for i, char in enumerate(min_str):
+        for string in strs:
+            if string[i] != char:
+                return min_str[:i]
+    
+    return min_str
+result = longest(strs)
+print(result)
+
+##4
+input_string = "abc"
+def string_permutations(s):
+    if len(s) == 0:
+        return []
+    if len(s) == 1:
+        return [s]
+    permutations = []
+    for i, char in enumerate(s):
+        remaining_chars = s[:i] + s[i+1:]
+        for perm in string_permutations(remaining_chars):
+            permutations.append(char + perm)
+    return permutations
+permutations = string_permutations(input_string)
+print(permutations)
